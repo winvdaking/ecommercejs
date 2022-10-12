943ca734e12f3b938525482f746eef67c01a1270
winvdaking <pro.dorianlopez@gmail.com>
2022-10-12 17:36:25 +0200

    add cart module

--- modules/cart.js
+++ modules/cart.js
@@ -1,5 +1,11 @@
 let panier = Array();
 
 export function addToCart(product){
-    panier.push(product);
+    panier.forEach((pdt) => {
+        if (pdt == product) {
+            panier[pdt].qte++;
+        }else{
+            panier.push([pdt, qte=1])
+        }
+    });
 }

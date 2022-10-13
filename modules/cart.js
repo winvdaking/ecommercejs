d9e2d41cb2edfda51fd938048ad47c8d9e681834
winvdaking <pro.dorianlopez@gmail.com>
2022-10-13 11:53:02 +0200

    add localStorage Cart

--- modules/cart.js
+++ modules/cart.js
@@ -2,6 +2,7 @@ import { genericCalc } from "./ui.js";
 import { removeAllChildNodes } from "./utils.js";
 
 export let panier = [];
+setInterval(localStorage.setItem('panier', panier), 3000);
 
 export function addToCart(product){
     if (panier.find(tab => tab.product.ref == product.ref)) {
@@ -15,4 +16,5 @@ export function emptyCart(){
     panier = [];
     removeAllChildNodes(document.getElementById('cart-content'));
     genericCalc(0, 0);
+    localStorage.removeItem('panier');
 }

683d9757009df3108ff2ac00d1d8d96c2d4bbc66
winvdaking <pro.dorianlopez@gmail.com>
2022-10-13 09:48:21 +0200

    fix cart

--- modules/cart.js
+++ modules/cart.js
@@ -1,11 +1,9 @@
 let panier = [];
 
 export function addToCart(product){
-    // bug
-    if (panier.find(pdt => pdt.ref === product.ref)) {
-        panier.find(pdt => pdt.ref === product.ref).qte+=1;
+    if (panier.find(tab => tab.product.ref == product.ref)) {
+        panier.find(tab => tab.product.ref == product.ref).qte+=1;
     }else{
         panier.push({product, qte: 1})
     }
-    console.log(panier);
 }
